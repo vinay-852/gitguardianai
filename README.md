0cbbd72b51b6eef08173056b3c662d062042cdb9
Vinay Pepakayala <vinaypepakayala@Vinays-MacBook-Air.local>
2025-06-17 15:18:19 +0530

    Clean Up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+---
+title: GITGuardianAI
+app_file: app.py
+sdk: gradio
+sdk_version: 5.34.0
+---
 # gitguardianai
--- app.py
+++ app.py
@@ -70,4 +70,4 @@ iface = gr.Interface(
 )
 
 if __name__ == "__main__":
-    iface.launch()
+    iface.launch(share=True)
